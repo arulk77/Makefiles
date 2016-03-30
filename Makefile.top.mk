020430c29b89fee1973089df6ab2be98bbc44156
Arul Subbiah <arul.subbiah@avagotech.com>
2016-03-29 17:09:43 -0700

    Modified makefile to include different target

--- Makefile.top.mk
+++ Makefile.top.mk
@@ -1,32 +1,35 @@
 ## This is the top level makefile for the design
 ifndef INCLUDE_TOP
-INCLUDE_TOP := $(HOME)/Makefiles/design
+INCLUDE_TOP := $(HOME)/Makefiles
 endif
 
-verdi_mk_file := $(INCLUDE_TOP)/Makefile.verdi.mk
-
-## Include the bsub command Makefile
-include $(INCLUDE_TOP)/Makefile.bsub.mk
+verdi_mk_file := $(INCLUDE_TOP)/design/Makefile.verdi.mk
 
 ## Variable for the help command
 define HLP
   TOP : This is the top level help
 endef
 
-MAKE_CMD := @$(MAKE) -f $(INCLUDE_TOP)/Makefile.bsub.mk
+MAKE_FILES := $(wildcard $(INCLUDE_TOP)/design/*.mk)
+
+MAKE_CMD := @$(MAKE) $(MFLAGS) -f $(INCLUDE_TOP)/bsub/Makefile.bsub.mk 
 
 ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## Targets for the top level
 ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 .PHONY: clean help this* verd*
 
-clean: verdi_clean
+## Clean everything
+clean:
+	$(foreach mfile,$(MAKE_FILES),@$(MAKE_CMD) -f $(mfile) $@;)
 
-help: this_help verdi_help
+## Print all the help menu
+help: 
+	$(foreach mfile,$(MAKE_FILES),@$(MAKE_CMD) -f $(mfile) $@;)
 
 this_help:
 	@echo "TOP : This includes all the help routine"
 
-verd%: 
-	$(MAKE_CMD) -f $(INCLUDE_TOP)/Makefile.verdi.mk $@
+ver%: 
+	$(MAKE_CMD) -f $(verdi_mk_file) $@
 
--- design/Makefile.verdi.mk
+++ design/Makefile.verdi.mk
@@ -26,14 +26,17 @@ endif
 
 ## Verdi tool specific options
 verdi_lib      := verdilib
-
 vcom_def_opt   := -ssy -ssv -sverilog -assert svaext -lib $(verdi_lib)
+verdi_def_opt  := -nologo -WorkMode hardwareDebug
 
+## If use specifies the option then add that as variable
 ifdef verdicom_opt
 verdicom_opt   := $(verdicom_opt) $(vcom_def_opt)
+else
+verdicom_opt   := $(vcom_def_opt)
 endif
 
-verdi_def_opt  := -nologo -WorkMode hardwareDebug
+## If verdi option is specified by the user then append
 ifdef verdi_opt
 verdi_opt      := $(verdi_opt) $(verdi_def_opt)
 else
@@ -49,8 +52,11 @@ endif
 verdi_clean:
 	rm -rf $(verdi_lib)* vericom* verdicomp* verdiLog
 
+## Verdicom and vericom are the same
+vericom: verdicom
+
 verdicom:
-	$(bsub_cmd) \vericom $(vcom_opt) -f $(file)
+	$(bsub_cmd) \vericom $(verdicom_opt) -f $(file)
 
 ## Invoke the verdi in ghi mode
 verdi: 
@@ -61,3 +67,7 @@ verdi_help:
 	@echo "$(THIS_FILE) : Usage for the verdi commandas are "
 	@echo "             : make file=<vlist_file_name> verdicom   --> To compile"
 	@echo "             : make fsdb=<fsdbfilename? rc_file=[rc file] vtop=[vtop file] verdi"
+
+## Clean and help are same as verdi commands
+clean: verdi_clean
+help: verdi_help
